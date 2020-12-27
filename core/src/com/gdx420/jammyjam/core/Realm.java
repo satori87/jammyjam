f8ff3a5ff101c557df5ee60ccf7e11e12a3d2a5b
satori87 <satori87@gmail.com>
2020-12-26 22:41:17 -0500

    Map exits

--- core/src/com/gdx420/jammyjam/core/Realm.java
+++ core/src/com/gdx420/jammyjam/core/Realm.java
@@ -14,34 +14,74 @@ public class Realm extends BaseRealm {
 
 	public static MapData[] mapData = new MapData[Shared.NUM_MAPS];
 
-	public static int curMap = 0;
+	public static int curMap = 1;
 	public static int loadedMapsCount = 0;
 
 	public Map map;
 
 	public Realm(Assets assets) {
 		super(assets);
-		realm = this;		
+		realm = this;
 	}
 
 	@Override
 	public void update() {
 
 	}
+	
+	public MapData mapData(int mapNum) {
+		return mapData[mapNum];
+	}
 
 	@Override
 	public void load() {
 		Log.debug("Loading Maps");
 		FileHandle f;
-		for(int i = 0; i < Shared.NUM_MAPS; i++) {
+		for (int i = 0; i < Shared.NUM_MAPS; i++) {
 			f = Gdx.files.local("maps/map" + i + ".map");
-			if(f != null && f.exists()) {
+			if (f != null && f.exists()) {
 				loadMap(i);
 				loadedMapsCount++;
 			} else {
 				mapData[i] = new MapData();
 			}
+			MapData m = mapData[i];
+			if(i <= 100 && i > 0) {
+				for (int e = 0; e < 4; e++) {
+					switch (e) {
+					case 0:
+						if(i > 10) {
+							m.exit[e] = i - 10;
+						} else {
+							m.exit[e] = 0;
+						}
+						break;
+					case 1:
+						if(i < 91) {
+							m.exit[e] = i + 10;
+						} else {
+							m.exit[e] = 0;
+						}
+						break;
+					case 2:
+						if(i % 10 > 1) {
+							m.exit[e] = i - 1;
+						} else {
+							m.exit[e] = 0;
+						}
+						break;
+					case 3:
+						if(i % 10 > 0) {
+							m.exit[e] = i + 1;
+						} else {
+							m.exit[e] = 0;
+						}
+						break;
+					}
+				}
+			}
 		}
+
 	}
 
 	@Override
