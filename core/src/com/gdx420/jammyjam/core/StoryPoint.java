0fb1d1a88400ba53671c5c1c3fccc422ccb2291d
MacAroniCap <codingzombie@gmail.com>
2020-12-27 00:18:47 -0500

    began adding storypoints...another checkin

---
+++ core/src/com/gdx420/jammyjam/core/StoryPoint.java
@@ -0,0 +1,13 @@
+package com.gdx420.jammyjam.core;
+
+public class StoryPoint {
+	public String name = new String();
+	public boolean active_sleep = false;
+	public boolean active_awake = false;
+	
+	public DialogData[] dialogs;
+	
+	public boolean onScreen = false;
+	public int x;
+	public int y;
+}
