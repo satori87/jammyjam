b8861d415d3c6652bd06d6011ec12241eba24d34
MacAroniCap <codingzombie@gmail.com>
2020-12-27 20:53:22 -0500

    merge fix

--- core/src/com/gdx420/jammyjam/core/Assets.java
+++ core/src/com/gdx420/jammyjam/core/Assets.java
@@ -9,10 +9,7 @@ import com.bg.bearplane.engine.BaseAssets;
 
 public class Assets extends BaseAssets {
 	
-	public static HashMap<String, Sound> sounds = new HashMap<String, Sound>();
-
-	public static HashMap<String, Sound> sounds = new HashMap<String, Sound>();
-	
+	public static HashMap<String, Sound> sounds = new HashMap<String, Sound>();	
 	
 	public void preload() {		
 		//add any customer preloading here
@@ -25,19 +22,7 @@ public class Assets extends BaseAssets {
 	}
 
 	public void load() {
-		File f = new File("assets/sfx");
-
-        // Populates the array with names of files and directories
-        //pathnames = f.list();
-
-        // For each pathname in the pathnames array
-        for (String pathname : f.list()) {
-            // Print the names of files and directories
-        	//Sound sound = manager.get("sfx/" + pathname);
-        	Sound sound = Gdx.audio.newSound(Gdx.files.local("sfx/" + pathname));
-        	sounds.put(pathname.substring(0,pathname.length()-4),sound);
-           // System.out.println(pathname.substring(0,pathname.length()-4));
-        }
+	
 		//add any custom loading here
 				File f = new File("music");
 
